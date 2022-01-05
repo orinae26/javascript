1d5598165228c9b7d88b04fc9862123a40710427
Orina <oryna.rich26@gmail.com>
2022-01-05 13:15:27 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,28 @@
+
+// arrays of fruits
+const fruits = ["Apple", "Orange", "Banana", "Pear"];
+
+fruits.push("Kiwi"); // add to the end of the array
+console.log(fruits);
+
+fruits.unshift("Strawberry"); // add to the beginning
+
+console.log(fruits);
+
+// remove from the end
+fruits.pop();// remove from the end
+console.log(fruits);
+
+// remove from the beginning
+fruits.shift();// remove from the beginning
+console.log(fruits);
+
+// remove from the middle
+fruits.splice(1, 1); // remove from the middle
+console.log(fruits);
+
+
+
+
+
+
